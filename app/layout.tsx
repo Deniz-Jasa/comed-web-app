9a173101373c9e508302b92328249e90b16873f1
Deniz Jasarbasic <denizj2828@gmail.comm>
2025-02-11 19:58:30 -0500

    added light fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
